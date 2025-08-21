d49dda3d5310654328682a48b489f0a7db343225
Rob Brazier <git+github@brzr.co>
2025-08-21 13:28:02 +0100

    stop dev mode if one of the processes gets killed

--- .web-ext/base-config.mjs
+++ .web-ext/base-config.mjs
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv';
+import fs from "fs";
 const { error, parsed: env } = dotenv.config();
 
 if (error) {
-	throw error;
+	if (error?.code === 'ENOENT') {
+		// copy the example .env config over for next time
+		fs.cpSync(".env.example", ".env")
+	} else {
+		console.error(error);
+	}
 }
 
 export const config = {
