b551747e90901e72824b72a05d2a375e42c547f8
Rob Brazier <git+github@brzr.co>
2025-08-21 14:07:01 +0100

    only override chromiumBinary if specified

--- .web-ext/chrome-config.mjs
+++ .web-ext/chrome-config.mjs
@@ -5,12 +5,20 @@ if (env.START_URLS) {
 	startUrl = env.START_URLS.split(",");
 }
 
+let chromeConfig = {};
+// doing it this way so that it'll use the default chromium binary instead of requiring it to be overridden
+if (env.CHROMIUM_BINARY) {
+	chromeConfig = {
+		chromiumBinary: env.CHROMIUM_BINARY
+	}
+}
+
 export default {
 	...config,
 	sourceDir: './distro/chrome',
 	run: {
 		target: ['chromium'],
-		chromiumBinary: env.CHROMIUM_BINARY || "chromium-browser",
+		...chromeConfig,
 		startUrl,
 	},
 };
